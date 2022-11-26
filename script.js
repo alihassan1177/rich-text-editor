f3678a86d26cc84d581b69d04efccd70d9e475d5
alihassan1177 <ah927481@gmail.com>
2022-11-26 20:15:48 +0500

    "More Progress"

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const componentBlock = document.querySelector("#component-block")
 const textEditor  = document.querySelector("#text-editor")
 const componentButtons = document.querySelectorAll("#component-btn")
+const code = document.querySelector("#code")
+
 
 componentButtons?.forEach(button => {
     button.addEventListener('click', (e)=>{
         componentBlock.classList.add("hidden")
+
+        const whichButton = button.dataset.component
+
+        if(whichButton == "heading"){
+            code.innerHTML += "This is a Heading"
+        }else if(whichButton == "image"){
+            code.innerHTML += "This is a Image"
+        }else if(whichButton == "link"){
+            code.innerHTML += "This is a Link"
+        }else if (whichButton == "paragraph"){
+            code.innerHTML += "This is a Paragraph"
+        }
+
     })
 })
 
 window.addEventListener('contextmenu', (event) => {
     if (event.target != textEditor) return
     event.preventDefault()
-
     componentBlockActionOnRightClick(event)
 })
 
