8bd1dc2cfc6343ca95b453a3fad4370e07b218c4
alihassan1177 <ah927481@gmail.com>
2022-11-26 19:59:10 +0500

    progress

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const componentBlock = document.querySelector("#component-block")
 const textEditor  = document.querySelector("#text-editor")
+const componentButtons = document.querySelectorAll("#component-btn")
+
+componentButtons?.forEach(button => {
+    button.addEventListener('click', (e)=>{
+        componentBlock.classList.add("hidden")
+    })
+})
+
+window.addEventListener('contextmenu', (event) => {
+    if (event.target != textEditor) return
+    event.preventDefault()
+
+    componentBlockActionOnRightClick(event)
+})
 
 
-window.oncontextmenu = function ()
-{
+function componentBlockActionOnRightClick(event){
     componentBlock.classList.remove("hidden")
-    return false;     // cancel default menu
+    componentBlock.style.top = `${event.clientY - 30}px`
+    componentBlock.style.left = `${event.clientX}px`
 }
